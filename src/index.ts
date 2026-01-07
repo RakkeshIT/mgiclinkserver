9ba1bfa177afeb17d12d08a47906678cf7cd7669
RakkeshFireSky <rakkeshkumar@firesky.cc>
2026-01-07 19:57:45 +0530

    Create Server

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+
+const PORT = process.env.PORT || 6000
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
