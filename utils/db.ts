43eaca0540556f50728806a9fcd2eb809d8e2ed2
RakkeshIT <rakkeshit@gmail.com>
2026-06-24 23:43:02 +0530

    Add mongoose dependency, update TypeScript root directory, and improve MongoDB connection logging

--- utils/db.ts
+++ utils/db.ts
@@ -1,21 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
   throw new Error("MONGODB_URI is Missing");
 }
 
-export async function connectDb() {
+export async function connectDb(): Promise<void> {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      serverSelectionTimeoutMS: 10000,
-    });
+    console.log("MONGODB_URI:", MONGODB_URI);
+    await mongoose.connect(MONGODB_URI);
 
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
-    process.exit(1); 
   }
 }
